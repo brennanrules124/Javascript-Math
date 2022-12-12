079ff5dcc088ae52f10663a5c70d19bb4dfe6b8d
brennanrules124 <70231079+brennanrules124@users.noreply.github.com>
2022-12-12 11:14:53 -0500

    Updated "addition.js"

--- addition.js
+++ addition.js
@@ -1,1 +1,26 @@
+var numbers = []; //Allows for any amount of numbers to add; No decimals or fractions though (See float files)
 
+//Get input from console
+
+/*Debug values*/
+numbers[0] = [4, 4, 6];
+numbers[1] = [5, 6, 7, 1];
+
+FormatVariables();
+
+//Flips the values; [0] = ones; [1] = tens, etc
+function FormatVariables(){ 
+    for(var i = 0; i < numbers.length; i++){
+        for(var j = 0; j < Math.floor(numbers[i].length / 2); j++){
+            let bufferVal = numbers[i][j];
+            numbers[i][j] = numbers[i][numbers[i].length - j - 1];
+            numbers[i][numbers[i].length - j - 1] = bufferVal;
+        }
+    }
+
+    console.log(numbers[0]);
+}
+
+function AddNumbers(){
+    
+}
