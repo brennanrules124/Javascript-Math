154513cc44af97e283db9090c1d6ca5b36f92247
brennanrules124 <70231079+brennanrules124@users.noreply.github.com>
2022-12-13 08:05:34 -0500

    Addition Finished

--- addition.js
+++ addition.js
@@ -2,11 +2,12 @@ var numbers = []; //Allows for any amount of numbers to add; No decimals or frac
 
 //Get input from console
 
-/*Debug values*/
+/*Inputs*/
 numbers[0] = [4, 4, 6];
 numbers[1] = [5, 6, 7, 1];
 
 FormatVariables();
+console.log(AddNumbers());
 
 //Flips the values; [0] = ones; [1] = tens, etc
 function FormatVariables(){ 
@@ -17,10 +18,34 @@ function FormatVariables(){
             numbers[i][numbers[i].length - j - 1] = bufferVal;
         }
     }
-
-    console.log(numbers[0]);
 }
 
 function AddNumbers(){
-    
+    let outputBuffer = 0;
+    for(var i = 0; i < GetLargestNumberIndex(); i++){
+        outputBuffer += (numbers[0][i] * Math.pow(10, i)) + (numbers[1][i] * Math.pow(10, i))
+    }
+
+    return outputBuffer;
+}
+
+//Also adds extra zeros to the end of the shortest arrays to make math terminate properly
+function GetLargestNumberIndex(){
+    let largestBuffer = 0;
+    for(var i = 0; i < numbers.length; i++){
+        if(numbers[i].length > largestBuffer){
+            largestBuffer = numbers[i].length;
+        }
+    }
+
+    for(var i = 0; i < numbers.length; i++){
+        if(numbers[i].length < largestBuffer){
+            //Ding ding ding
+            for(var j = numbers[i].length; j < largestBuffer; j++){
+                numbers[i][j] = 0;
+            }
+        }
+    }
+
+    return largestBuffer;
 }
